e4976c8b33d9c577de8d02b428216752fe3b2fb1
Emilie <emilie@emilies-macbook-pro.home>
2021-12-24 16:23:02 +0100

    remove rect from month

--- src/month-picker.tsx
+++ src/month-picker.tsx
@@ -24,19 +24,18 @@ const MonthPicker: React.FC<MonthPickerProps> = ({
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
+          width="14"
+          height="14"
           fill="currentColor"
           viewBox="0 0 256 256"
         >
-          <rect width="256" height="256" fill="none"></rect>
           <polyline
             points="160 208 80 128 160 48"
             fill="none"
             stroke="currentColor"
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth="24"
+            strokeWidth="14"
           ></polyline>
         </svg>
       </button>
@@ -57,19 +56,18 @@ const MonthPicker: React.FC<MonthPickerProps> = ({
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
+          width="14"
+          height="14"
           fill="currentColor"
           viewBox="0 0 256 256"
         >
-          <rect width="256" height="256" fill="none"></rect>
           <polyline
             points="96 48 176 128 96 208"
             fill="none"
             stroke="currentColor"
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth="24"
+            strokeWidth="14"
           ></polyline>
         </svg>
       </button>
