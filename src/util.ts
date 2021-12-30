270a35fe30627d05aa0c45a02c4e14f83031b2ba
Emilie <emilie@emilies-macbook-pro.home>
2021-12-30 16:57:56 +0100

    change date

--- src/util.ts
+++ src/util.ts
@@ -69,7 +69,7 @@ export const getDatesOfMonth = (date: Date): { d: Date; active: boolean; inMonth
 
   for (let i = 0; i < lastDayOfMonth.getDate(); i++) {
     dates.push({
-      d: new Date(date.getFullYear(), date.getMonth(), i + 2),
+      d: new Date(date.getFullYear(), date.getMonth(), i + 1),
       active: true,
       inMonth: true,
     });
